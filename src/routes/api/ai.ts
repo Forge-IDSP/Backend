ea7c42fecd8e8c89063206765d454f379f1c5f63
Kyle You <kyou4@my.bcit.ca>
2025-10-27 15:23:28 -0700

    test

--- src/routes/api/ai.ts
+++ src/routes/api/ai.ts
@@ -11,24 +11,63 @@ export const aiRoute = new Hono();
 aiRoute.get("/test", async (c)=>{
 const response = await ai.models.generateContent({
   model: "gemini-2.0-flash",
-  contents: "List 4 trending BC skilled trdaes job.",
-  config: {
-    responseMimeType: "application/json",
-    responseSchema: {
-       type: "array",
-        items: {
-         type: "object",
-        properties: {
-        jobTitle: { type: "string" },
+contents: "List 5 guided steps to becoming an electrician BC. Question from AI should be something like this 'Do you want to continue to know about' instead of do you want to continue to step 1, 2, 3. Each step should include a short description and a yes/no question asking if the user wants to continue.",
+config: {
+  responseMimeType: "application/json",
+  responseSchema: {
+    type: "array",
+    items: {
+      type: "object",
+      properties: {
+        stepNumber: { type: "integer" },
         description: { type: "string" },
-        },
-        required: ["jobTitle", "description"],
-         },
+        question: { type: "string" },
+      },
+      required: ["stepNumber", "description", "question"],
     },
   },
+},
 });
 const data = response.text
 console.log(data)
 const jobs = JSON.parse(data!); // jobs is an array
 return c.json({ jobs });
 })
+
+aiRoute.post("/chat", async(c:any)=>{
+
+  const { type, text } = await c.req.json();
+
+  const aiResponse = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: text,
+    config: {
+      responseMimeType: "application/json",
+      responseSchema: {
+        type: "string",
+      },
+    },
+  });
+
+return c.json({ type:"assistant_text", text:aiResponse }, 200)
+
+
+
+  
+
+
+  // Step 1. I fetch your chat response from the user
+
+  // Step 2. I validate if it's relevant to the conversation, then if it is I would prompt that to AI
+
+  // Step 3. I get the response back from AI, and I send it over
+
+
+
+
+
+  // Alterates: Badges, If we are doing badges we need to have a database with semething like user -> badges -> true/false
+
+  // If the user prompts like 3 times, we give them one badge or what condition, 
+})
+
