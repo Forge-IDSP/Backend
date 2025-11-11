a88335ae286b5ca9af71ffce4ecba115f8caac5d
Kyle You <kyou4@my.bcit.ca>
2025-11-10 16:50:25 -0800

    test

--- src/routes/api/ai.ts
+++ src/routes/api/ai.ts
@@ -6,3 +6,5 @@ export const aiRoute = new Hono();
 aiRoute.post("/chats", async (c: any) => {
   return await aiController.getChatResponse(c);
 });
+
+aiRoute.post("/initialize", async (c: any) => {});
